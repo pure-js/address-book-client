beff9213b0d4071a3b3737ab1f14a064abe7ba46
pure-js <3690368+pure-js@users.noreply.github.com>
2023-08-30 02:16:35 +0400

    fix: default template to real

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Main Page</h1>
+
+<a href="/contacts">Contacts page</a>
